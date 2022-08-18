d1f9b65b2bd014f06ebbecf609c18b2f0818d3fd
Ana Laura <92990372+lauraxsantos@users.noreply.github.com>
2022-08-18 15:20:14 -0300

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,16 +1,24 @@
-def insertionSort(lista):
+lista = []
+from random import randint
     
-    for i in range(1, len(lista)):
-        key = lista[i]
-        j = i - 1
+while len(lista) < 30:
+    
+    item = randint(0,30)
+    if item % 2 != 0:
+        lista.append(item)
+    
+print(lista)
+
+for i in range(1, len(lista)):
+    key = lista[i]
+    j = i - 1
         
-        while j>= 0 and key < lista[j]:
-            lista[j+1] = lista[j]
-            j = j - 1
+    while j>= 0 and key < lista[j]:
+        lista[j+1] = lista[j]
+        j = j - 1
             
-        lista[j+1] = key
+    lista[j+1] = key
         
 
-array = [5, 6, 1, 9, 7]
-insertionSort(array)
-print(array)
+print(lista)
+
