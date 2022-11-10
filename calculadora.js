54af2403787ee6011845b45ac0d687e496b7d668
Ana Laura <92990372+lauraxsantos@users.noreply.github.com>
2022-11-10 11:39:13 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+var num1 = prompt('Digite um número: ');
+var num2 = prompt('Digite o segundo número: ');
+var op = prompt('Escolha um operador aritmético(* + / -): ');
+var result;
+
+if (op === '/') {
+  result = parseInt(num1) / parseInt(num2);
+} else if (op === '+') {
+  result = parseInt(num1) + parseInt(num2);
+} else if (op === '-') {
+  result = parseInt(num1) - parseInt(num2);
+} else if (op === '*') {
+  result = parseInt(num1) * parseInt(num2);
+} else {
+  console.log('Você digitou um operador inválido');
+}
+
+console.log(result);
